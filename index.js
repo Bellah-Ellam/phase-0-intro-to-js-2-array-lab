f6e9676197d0295a5baf4ae2197171fc0f2a53fc
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-14 21:43:17 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) { 
+    cats.push('Ralph')}
+
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    var newArray = [...cats, "Broom"]
+    return newArray
+}
+
+function prependCat(name){
+    var arrayNew = ["Arnold", ...cats]
+    return arrayNew
+}
+function removeLastCat() {
+    var lastSlice = cats.slice(0,cats.length-1)
+    return lastSlice
+}
+
+function removeFirstCat(){
+    var firstSlice = cats.slice(1)
+    return firstSlice
+}
